340bc6c0a0609b79a4b950eff697e20951daaa37
Komal Thareja <kthare10@renci.org>
2024-08-07 11:31:30 -0400

    added install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo dnf install -y git
+sudo dnf install -y python3.11
+curl -O https://bootstrap.pypa.io/get-pip.py
+sudo python3.11 get-pip.py
+sudo dnf groupinstall -y "Development Tools"
+pip3.11 -r requirements.txt
