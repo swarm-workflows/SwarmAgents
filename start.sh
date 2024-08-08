0a523fb6b8f8a8c1270000a6e70f9102b4d97604
Komal Thareja <kthare10@renci.org>
2024-08-08 15:51:46 -0400

    update the start script

--- start.sh
+++ start.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-# Run the Kafka cleanup script
-python kafka_cleanup.py
-python task_generator.py
-
-# Define the number of agents
-num_agents=3
-base_id=0
-
-# Create an array to store PIDs
-pids=()
-
-# Loop through the agent IDs and start each one in the background
-for agent_id in $(seq 1 $num_agents); do
-  ag_id=$((agent_id + base_id))
-  python main.py --agent_id $ag_id &
-  pids+=($!) # Store the PID of the background process
-done
-
-# Save the PIDs to a file
-echo "${pids[@]}" > agent_pids.txt
-
-echo "Started $num_agents agents."
+python3.11 main-pbft.py 1 100
+python3.11 main-pbft.py 2 100
 
