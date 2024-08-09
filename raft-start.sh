a609789cac69719ac4c1414bded51bd1180e48c7
Komal Thareja <kthare10@renci.org>
2024-08-09 15:00:02 -0400

    update readme and plotter

--- raft-start.sh
+++ raft-start.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Run the Kafka cleanup script
-python kafka_cleanup.py
+python3.11 kafka_cleanup.py
 
 # Define the number of agents
 num_agents=4
 
 num_tasks=100
 
-python main-raft.py 1 $num_agents $num_tasks &
-python main-raft.py 2 $num_agents $num_tasks &
-#python main-raft.py 3 $num_agents $num_tasks &
-#python main-raft.py 4 $num_agents $num_tasks &
-#python main-raft.py 5 $num_agents $num_tasks &
-#python main-raft.py 6 $num_agents $num_tasks &
-#python main-raft.py 7 $num_agents $num_tasks &
-#python main-raft.py 8 $num_agents $num_tasks &
-#python main-raft.py 9 $num_agents $num_tasks &
+python3.11 main-raft.py 1 $num_agents $num_tasks &
+python3.11 main-raft.py 2 $num_agents $num_tasks &
+#python3.11 main-raft.py 3 $num_agents $num_tasks &
+#python3.11 main-raft.py 4 $num_agents $num_tasks &
+#python3.11 main-raft.py 5 $num_agents $num_tasks &
+#python3.11 main-raft.py 6 $num_agents $num_tasks &
+#python3.11 main-raft.py 7 $num_agents $num_tasks &
+#python3.11 main-raft.py 8 $num_agents $num_tasks &
+#python3.11 main-raft.py 9 $num_agents $num_tasks &
