872c1223257cfeb84d89d7a1c1463fc34bc009c3
Komal Thareja <kthare10@renci.org>
2024-08-15 11:00:23 -0400

    set the message type in builder

--- swarm/comm/messages/message_builder.py
+++ swarm/comm/messages/message_builder.py
@@ -11,16 +11,22 @@ from swarm.comm.messages.task_status import TaskStatus
 class MessageBuilder:
     @staticmethod
     def from_dict(message: dict) -> Tuple[HeartBeat, Proposal, Prepare, Commit, TaskStatus]:
-        message_type = message.get('message_type')
+        message_type = MessageType(message.get('message_type'))
+
         if message_type == MessageType.HeartBeat:
             return HeartBeat.from_dict(message)
+
         elif message_type == MessageType.Proposal:
             return Proposal.from_dict(message)
+
         elif message_type == MessageType.Prepare:
             return Prepare.from_dict(message)
+
         elif message_type == MessageType.Commit:
             return Commit.from_dict(message)
+
         elif message_type == MessageType.TaskStatus:
             return TaskStatus.from_dict(message)
+
         else:
             raise MessageException(f"Unsupported Message: {message}")
