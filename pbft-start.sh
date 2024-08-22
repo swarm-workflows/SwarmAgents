7807bcf76735759fbe0e39cc33ecbd1391d15edd
Komal Thareja <kthare10@renci.org>
2024-08-22 11:58:48 -0400

    update results

--- pbft-start.sh
+++ pbft-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python main-pbft.py 1 100 &
-python main-pbft.py 2 100 &
+python3.11 main-pbft.py 1 100 &
+python3.11 main-pbft.py 2 100 &
 #python3.11 main-pbft.py 3 100 &
 #python3.11 main-pbft.py 4 100 &
 #python3.11 main-pbft.py 5 100 &
