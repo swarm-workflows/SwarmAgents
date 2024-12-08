8c785046a3a5623e7cf0af89ea7ed66b6baaf92f
Komal Thareja <kthare10@renci.org>
2024-12-08 07:28:40 -0500

    update result dir

--- raft-start.sh
+++ raft-start.sh
@@ -5,9 +5,10 @@ python3.11 kafka_cleanup.py --topic agent
 rm -rf raft
 mkdir -p raft
 # Define the number of agents
-num_agents=5
+num_agents=10
 
 # Launch the Python commands for each agent
 for i in $(seq 0 $(($num_agents - 1))); do
     python3.11 main-raft.py $i $num_agents 100 &
+    sleep 1
 done
